ce2a8a777e7e7bcd25d8ca29162bafeb37e418dc
Bert Schols <bert@metatalk.be>
2016-01-08 16:12:10 +0100

    Projects

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,17 +7,39 @@ $(function() {
     }
   });
 
+  $('.site-nav__toggle').click(function(e) {
+    $('.site-nav__inner').toggleClass('is-out')
+  });
+
   // onload
   if(document.body.clientWidth >= 768) {
     $('video').attr('autoplay', true);
   }
-
   // If you want to autoplay when the window resized wider than 780px
   // after load, you can add this:
-
   $(window).resize(function() {
     if(document.body.clientWidth >= 768) {
       $('video').attr('autoplay', true);
     }
   });
+
+  $('.project-detail__image').hover(function() {
+    $(this).addClass('is-hover');
+  },function() {
+    $(this).removeClass('is-hover');
+  });
+
+  $('.btn--project').click(function(e) {
+    $('body').addClass('is-modal-open');
+    var hash = $(this).attr('href').substring(2);
+    $('.project-detail#'+hash).addClass('is-open');
+  });
+
+  $('.project-detail__close').click(function(e) {
+    e.preventDefault();
+    history.pushState({}, '', './wat-we-doen.html');
+    $('body').removeClass('is-modal-open');
+    $('.project-detail').removeClass('is-open');
+  });
+
 });
